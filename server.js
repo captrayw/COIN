d513883f13c528c639e2ca402e12a516d3ede6ba
Petray Wu <petray.wu@castalia-advisors.com>
2026-04-16 16:31:59 -0400

    Initial commit: coin flip game

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  fs.createReadStream(path.join(__dirname, 'coin-flip.html')).pipe(res);
+}).listen(PORT, () => console.log(`Listening on port ${PORT}`));
